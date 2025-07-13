473e650d7c3939d535f145c33251bd6e0d936c1d
Nayem Ahmed <nishanahmed13913@gmail.com>
2025-07-13 23:27:12 +0600

    setup routing with react-router v6+

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,5 +1,9 @@
 import App from "@/App";
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom"; 
+import AddBook from './../pages/AddBook';
+import BorrowSummery from './../pages/BorrowSummery';
+import AllBook from "@/pages/AllBook";
+import Home from "@/pages/Home";
 
 
 const router = createBrowserRouter([
@@ -7,7 +11,22 @@ const router = createBrowserRouter([
         path: '/',
         Component: App,
         children: [
-
+            {
+                index: true,
+                Component: Home
+            },
+            {
+                path: 'add_book',
+                Component: AddBook
+            },
+            {
+                path: 'borrow_summery',
+                Component: BorrowSummery
+            },
+            {
+                path: 'all_book',
+                Component: AllBook
+            }
         ]
     }
 ])

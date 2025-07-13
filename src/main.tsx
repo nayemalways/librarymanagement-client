ec181dc94d69f6c0bec7b90f0069931839a6a06e
Nayem Ahmed <nishanahmed13913@gmail.com>
2025-07-13 23:28:31 +0600

    wrapped hole application with Redux Provider and Shadcn theme provider

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,16 @@ import './index.css'
 import { ThemeProvider } from './theme/theme.tsx'
 import { RouterProvider } from 'react-router-dom'
 import router from './routes/routes.ts'
+import { Provider } from 'react-redux'
+import store from './redux/store.ts'
+ 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-       <RouterProvider router={router} />
-    </ThemeProvider>
-    
+    <Provider store={store}>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </Provider>
   </StrictMode>,
 )

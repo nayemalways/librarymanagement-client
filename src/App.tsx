3538c82c6ffe50b59a867514322e898aeeb8e1d9
Nayem Ahmed <nishanahmed13913@gmail.com>
2025-07-13 23:29:39 +0600

    set routing child component using Outlet components from react-ruter-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from './components/module/navbar/Navbar';
-
+ 
 const App = () => {
   return (
-    <div>
+    <>
       <Navbar />
       <Outlet />
-    </div>
+    </>
   );
 };
 

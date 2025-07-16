9599ceb2b2ba54ee4cc1c749ad960c86c94f5bbe
Nayem Ahmed <nishanahmed13913@gmail.com>
2025-07-17 00:10:26 +0600

    connect api data

--- src/components/module/DataTable.tsx
+++ src/components/module/DataTable.tsx
@@ -2,14 +2,33 @@ import {  Edit } from 'lucide-react';
 import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '../ui/table';
 import ConfirmModal from './app_component/ConfirmModal';
 import { Badge } from '../ui/badge';
-import { data } from '@/data/data';
 import BorrowModal from './app_component/BorrowModal';
+import { useGetBookQuery } from '@/redux/rtkQuery/apiSlice';
+import { Skeleton } from '../ui/skeleton';
 
 const DataTable = () => {
-     
+    const { data, isLoading, error } = useGetBookQuery({});
+
+    if(isLoading) {
+        return(
+            <div className='h-screen w-full flex justify-center items-center flex-col gap-4'>
+                <Skeleton className="h-[20px] w-5xl rounded-full" />
+                <Skeleton className="h-[20px] w-5xl rounded-full" />
+                <Skeleton className="h-[20px] w-5xl rounded-full" />
+                <Skeleton className="h-[20px] w-5xl rounded-full" />
+                <Skeleton className="h-[20px] w-5xl rounded-full" />
+                <Skeleton className="h-[20px] w-5xl rounded-full" />
+                <Skeleton className="h-[20px] w-5xl rounded-full" />
+                <Skeleton className="h-[20px] w-5xl rounded-full" />
+                <Skeleton className="h-[20px] w-5xl rounded-full" />
+                <Skeleton className="h-[20px] w-5xl rounded-full" />
+                <Skeleton className="h-[20px] w-5xl rounded-full" />
+            </div>
+        )
+    }
+
     return (
         <Table  className='max-w-5xl h-auto m-auto rounded bg-slate-300 dark:bg-slate-800 md:mt-15'>
-            <TableCaption>A list of all recent books.</TableCaption>
             <TableHeader>
                 <TableRow>
                     <TableHead>Id</TableHead>
@@ -26,8 +45,7 @@ const DataTable = () => {
             <TableBody>
                 {
                     data?.data.map((book, i) => (
-                        <>
-                            <TableRow >
+                            <TableRow key={i}>
                                 <TableCell className="font-medium">{i+1}</TableCell>
                                     <TableCell>{book?.title}</TableCell>
                                     <TableCell>{book?.author}</TableCell>
@@ -55,7 +73,6 @@ const DataTable = () => {
                                         <ConfirmModal/>
                                     </TableCell>
                             </TableRow>
-                        </>
                     ))
                 }
             </TableBody>
